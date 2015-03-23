fc89d9d16a541789e0b07f9ddc1f12f418a07c4a
Liu Cong <innerr@gmail.com>
2015-03-23 16:55:24 +0800

    Done: pkg measure

--- src/ember/measure/data.go
+++ src/ember/measure/data.go
@@ -4,6 +4,76 @@ import (
 	"time"
 )
 
+func (p *MeasureData) After(time int64) MeasureData {
+	data := *p
+	i := len(data) - 1
+	if time > data[i].Time {
+		return NewMeasureData(0)
+	}
+
+	for ; i >= 0; i-- {
+		if data[i].Time == time {
+			break
+		}
+	}
+
+	after := MeasureData(data[i:])
+	return after.Copy()
+}
+
+func (p *MeasureData) Copy() MeasureData {
+	result := NewMeasureData(len(*p))
+	for i, it := range (*p) {
+		result[i] = it.Copy()
+	}
+	return result
+}
+
+func (p *MeasureData) LastTime() int64 {
+	return (*p)[len(*p) - 1].Time
+}
+
+func (p *MeasureData) Padding(count int) {
+	data := *p
+	if count >= len(data) {
+		*p = NewMeasureData(len(data))
+	} else {
+		for i := 0; i < count; i++ {
+			*p = append(*p, NewSpanData())
+		}
+		*p= (*p)[count:]
+	}
+}
+
+func (p *MeasureData) Merge(time int64, name string, value int64) {
+	data := *p
+	last := data[len(data) - 1]
+
+	if last.Time == 0 {
+		last.Time = time
+	}
+
+	for i := len(data) - 1; i >= 0; i-- {
+		if data[i].Time != time {
+			continue
+		}
+		data[i].Merge(name, value)
+		return
+	}
+
+	panic("Measure.Record: unexpect")
+}
+
+func NewMeasureData(count int) MeasureData {
+	data := make([]*SpanData, count)
+	for i, _ := range data {
+		data[i] = NewSpanData()
+	}
+	return data
+}
+
+type MeasureData []*SpanData
+
 func (p *SpanData) Merge(name string, value int64) {
 	if _, ok := p.Data[name]; !ok {
 		p.Data[name] = &SpecData{}
