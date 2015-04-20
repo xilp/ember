5e1df81cb430fc3be985b1d2e94376bd46f16360
Liu Cong <innerr@gmail.com>
2015-04-21 01:43:07 +0800

    Simpler rpc test

--- src/ember/http/rpc/rpc_test.go
+++ src/ember/http/rpc/rpc_test.go
@@ -1,61 +1,21 @@
 package rpc
 
 import (
-	"sync"
 	"testing"
 	"time"
 )
 
-var testServerOnce sync.Once
-var testClientOnce sync.Once
-
-var testServer *Server
-var testClient *Client
-
-func newTestServer() *Server {
-	f := func() {
-		testServer = NewServer()
-		go testServer.Run(11182)
-	}
-	testServerOnce.Do(f)
-	return testServer
-}
-
-func newTestClient() *Client {
-	f := func() {
-		testClient = NewClient("http://127.0.0.1:11182")
-	}
-	testClientOnce.Do(f)
-	return testClient
-}
-
-type Integer int;
-
-func (p *Integer) Larger(a, b int) (bool, error) {
-	return a < b, nil
-}
-
-func (p *Integer) Add(a, b int) (int, error) {
-	return a + b, nil
-}
-
-func (p *Integer) Trait() map[string][]string {
-	m := map[string][]string {
-	"Larger": []string{"a", "b"},
-	"Add": []string{"a", "b"},
-	}
-	return m
-}
-
 func TestRpc(t *testing.T) {
 	var a Integer
 
-	s := newTestServer()
+	s := NewServer()
 	s.Reg(&a)
+
+	go s.Run(11182)
 	time.Sleep(time.Millisecond)
 
-	c := newTestClient()
-	
+	c := NewClient("http://127.0.0.1:11182")
+
 	type B struct {
 		Larger func(a, b int)(bool, error)	
 		Add func(a, b int)(int, error)	
@@ -74,3 +34,22 @@ func TestRpc(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func (p *Integer) Larger(a, b int) (bool, error) {
+	return a < b, nil
+}
+
+func (p *Integer) Add(a, b int) (int, error) {
+	return a + b, nil
+}
+
+func (p *Integer) Trait() map[string][]string {
+	m := map[string][]string {
+	"Larger": []string{"a", "b"},
+	"Add": []string{"a", "b"},
+	}
+	return m
+}
+
+type Integer int;
+
